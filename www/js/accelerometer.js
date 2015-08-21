6efc6d0fc7786c89e2b403098de8ba5adfd2ac35
kritika-pattalam <kritika.pattalam@gmail.com>
2015-08-21 16:11:01 -0400

    Init commit
    Added start and stop for acceleration

--- www/js/accelerometer.js
+++ www/js/accelerometer.js
@@ -11,13 +11,20 @@ var watchID = null;
             function startWatch(){
                 var options = { frequency: 1000 }; // Update every 1 seconds
             //OTHER methods getCurrentAcceleration, clearWatch()
+           
             watchID =navigator.accelerometer.watchAcceleration(onSuccess,onError,options);
+            
+            
 
             }
             function stopWatch() {
                 if (watchID) {
+                    
                 navigator.accelerometer.clearWatch(watchID);
                 watchID = null;
+
+                document.getElementById('stopped').innerHTML = "No longer watching your acceleration.";
+               
                 }
             }
         function onSuccess(acceleration) {
@@ -25,6 +32,7 @@ var watchID = null;
             document.getElementById('y').innerHTML = acceleration.y;
             document.getElementById('z').innerHTML = acceleration.z;
             document.getElementById('timestamp').innerHTML= acceleration.timestamp;
+            document.getElementById('stopped').innerHTML = "Watching your acceleration";
         }
         function onError(error) {
             alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
